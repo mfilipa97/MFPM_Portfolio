64032d08c4edbd2d8aef3e1cfd61dde1439900d8
mfpm97 <marrian1997@hotmail.com>
2025-02-22 19:34:23 +0000

    Fixed navbar scroll to

--- src/components/Navbar/navbar.jsx
+++ src/components/Navbar/navbar.jsx
@@ -1,27 +1,25 @@
-import "./navbar.css"
+import { Link } from "react-scroll";
+import "./navbar.css";
 
+const Navbar = () => {
+    return (
+        <div className="navbar">
+            <ul className="nav_menu">
+                <li>
+                    <Link to="home" smooth={true} duration={200}>Home</Link>
+                </li>
+                <li>
+                    <Link to="about"  smooth={true} duration={200}>About</Link>
 
-const Navbar = () =>{
-    return(
-        <div className = "navbar">
-            <ul className = "nav_menu">
-                <li>About Me</li>
+                </li>
                 <li>Stacks</li>
-                <li>Projects</li>
+                <li>
+                    <Link to="projects" smooth={true} duration={200}>Projects</Link>
+                </li>
                 <li>Contact Me</li>
-
-
             </ul>
-
-
-
         </div>
-    )
+    );
+};
 
-
-
-
-}
-
-
-export default Navbar
+export default Navbar;
--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import GlobalProvide from 'vite-plugin-global-provide'
-
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    GlobalProvide({
-      $: 'jquery',
-      jQuery: 'jquery',
-      'window.jQuery': 'jquery',
-    }),
   ],
 })
